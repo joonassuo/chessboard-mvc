815048a47d1006c7485094257bbc9cb8167ba59b
Joonas <joonas.suonpera@gmail.com>
2019-06-07 13:25:33 +0300

    render pieces working

--- App/Controller/Controller.js
+++ App/Controller/Controller.js
@@ -1,17 +1,59 @@
 //-------------------------START GAME---------------------------------------
 
-// function to put pieces to starting position
-piecesToStartingPosition = () => {
-    for ( let i = 0; i < chessboard.length; i++ ) {
-        $('#grid')
-            .find('#' + chessboard[i].square)
-            .css('background-image', 'url(' + chessboard[i].image + ')');
+
+var Controller = {
+    
+    generatePieceMethod(color, type, square) {
+        var piece = {
+            color: color,
+            type: type,
+            image: Model.pieceTypes.find(o => o.color === color && o.type === type).image,
+            square: square
+        }
+        Model.chessboard.push(piece);
+    },
+
+
+    startGameMethod() {
+        // generate all pieces in starting positions and push to model
+        // ALL PAWNS
+        for(let i = 97; i < 105; i++){
+            this.generatePieceMethod( 'white', 'pawn', String.fromCharCode(i) + 2);
+            this.generatePieceMethod( 'black', 'pawn', String.fromCharCode(i) + 7);
+        }
+        // WHITE
+        this.generatePieceMethod('white', 'rook', 'a1');
+        this.generatePieceMethod('white', 'rook', 'h1');
+        this.generatePieceMethod('white', 'knight', 'b1');
+        this.generatePieceMethod('white', 'knight', 'g1');
+        this.generatePieceMethod('white', 'bishop', 'c1');
+        this.generatePieceMethod('white', 'bishop', 'f1');
+        this.generatePieceMethod('white', 'queen', 'd1');
+        this.generatePieceMethod('white', 'king', 'e1');
+        // BLACK
+        this.generatePieceMethod('black', 'rook', 'a8');
+        this.generatePieceMethod('black', 'rook', 'h8');
+        this.generatePieceMethod('black', 'knight', 'b8');
+        this.generatePieceMethod('black', 'knight', 'g8');
+        this.generatePieceMethod('black', 'bishop', 'c8');
+        this.generatePieceMethod('black', 'bishop', 'f8');
+        this.generatePieceMethod('black', 'queen', 'd8');
+        this.generatePieceMethod('black', 'king', 'e8');
+    },
+
+
+    renderPiecesMethod() {
+        const array = Model.chessboard.slice();
+        
+        array.forEach((element) => {   
+            var img = document.createElement('img');         
+            img.setAttribute('src', element.image);
+            document.querySelector('#' + element.square).appendChild(img);
+        });               
     }
 }
 
 
-// functions on start of game
-startGame = () => {
-    piecesToStartingPosition();
-}
+
+
 
---
+++ App/Model/Model.js
@@ -0,0 +1,82 @@
+var Model = {
+
+    chessboard: [],
+
+    piece: {
+        color: '',
+        type: '',
+        image: '',
+        square: ''
+    },
+
+    pieceTypes: [{
+            color: 'white',
+            type: 'pawn',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/800px-Chess_plt45.svg.png',
+        },
+        {
+            color: 'white',
+            type: 'rook',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/68px-Chess_rlt45.svg.png'
+        },
+        {
+            color: 'white',
+            type: 'knight',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/68px-Chess_nlt45.svg.png'
+        },
+        {
+            color: 'white',
+            type: 'bishop',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png'
+        },
+        {
+            color: 'white',
+            type: 'queen',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/68px-Chess_qlt45.svg.png'
+        },
+        {
+            color: 'white',
+            type: 'king',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/68px-Chess_klt45.svg.png'
+        },
+        {
+            color: 'black',
+            type: 'pawn',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/800px-Chess_pdt45.svg.png'
+        },
+        {
+            color: 'black',
+            type: 'rook',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png'
+        },
+        {
+            color: 'black',
+            type: 'knight',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png'
+        },
+        {
+            color: 'black',
+            type: 'bishop',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/68px-Chess_bdt45.svg.png'
+        },
+        {
+            color: 'black',
+            type: 'queen',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/68px-Chess_qdt45.svg.png'
+        },
+        {
+            color: 'black',
+            type: 'king',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/68px-Chess_kdt45.svg.png'
+        }
+    ],
+
+    gameStatus: {
+        toPlay: 'white'
+    },
+
+    move: {
+        startSquare: '',
+        endSquare: '',
+    }
+}
